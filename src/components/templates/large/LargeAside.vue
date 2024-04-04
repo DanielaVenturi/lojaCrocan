b9b7dda7edd664d07ae9d9149787e38528d63f3a
DanielaVenturi <danielaventuri14@gmail.com>
2024-04-04 09:05:31 -0300

    LargeAside e LargeHeader iniciados

--- src/components/templates/large/LargeAside.vue
+++ src/components/templates/large/LargeAside.vue
@@ -1,3 +1,63 @@
 <template>
-  <h2>Large Aside</h2>
+  <div class="Navbar">
+    <nav class="NavbarItems">
+      <div class="NavbarTitle">
+        Crocant
+      </div>
+      <div class="NavbarLink">
+        Feminino
+      </div>
+      <div class="NavbarLink">
+        Masculino
+      </div>
+      <div class="NavbarLink">
+        Infantil
+      </div>
+      <div class="NavbarLink">
+        Pins
+      </div>
+      <div class="NavbarLink">
+        ProTrabalho
+      </div>
+    </nav>
+  
+    
+  </div>
+  <hr>
 </template>
+<style scoped>
+hr{
+  background-color: #864EFF;
+  padding: 3px;
+}
+
+.Navbar {
+  background-color: #fff;
+  display: flex;
+  padding: 16px;
+  font-family: sans-serif;
+  color: #864EFF;
+
+}
+
+.NavbarItems {
+  display: flex;
+}
+
+.NavbarLink {
+margin-right: 10%;
+margin-left: 20%;
+ font-size: x-large;
+}
+
+.NavbarItemsRight {
+  margin-left: auto;
+}
+.NavbarTitle{
+  margin-right: 8px;
+  font-family: 'Cherry Bomb One';
+  font-size: x-large;
+}
+
+
+</style>
--- src/components/templates/large/LargeHeader.vue
+++ src/components/templates/large/LargeHeader.vue
@@ -1,4 +1,44 @@
 <template>
-    <h2>Large Header</h2>
-  </template>
-  
+  <div class="Navbar">
+    <nav class="NavbarItems">
+ 
+     
+     
+    
+    </nav>
+    <nav class="NavbarItems NavbarItemsRight">
+      <div class="NavbarLink">
+        Entrar
+      </div>
+    
+    </nav>
+    
+  </div>
+  <hr>
+</template>
+<style scoped>
+
+.Navbar {
+  background-color: #864EFF;
+  display: flex;
+  padding: 7px;
+  font-family: sans-serif;
+  color: #fff;
+
+}
+
+.NavbarItems {
+  display: flex;
+}
+
+.NavbarLink {
+ padding-right: 8px;
+}
+
+.NavbarItemsRight {
+  margin-left: auto;
+}
+
+
+
+</style>
